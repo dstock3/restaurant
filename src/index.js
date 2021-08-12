2b5022de4d859ab1f4d8361bcd32f4ea0ada3ce5
David Stockdale <dstock3@gmail.com>
2021-08-11 23:10:36 -0500

    Added placeholder image and landing page copy.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import { pageLoad } from './landingPage'
 pageLoad()
 
-console.log(content)
+let landingPage = elementBuilder("section", "landing-page", content);
+let header = elementBuilder("header", "page-head",  landingPage);
+header.textContent = "Vito's Pizzeria"
+let article = elementBuilder("article", "landing-article", landingPage);
+let landingImage = elementBuilder("img", "landing-image", landingPage);
+landingImage.src = ``;
+
 
--- src/landingPage.js
+++ src/landingPage.js
@@ -10,13 +10,16 @@ const pageLoad = () => {
     
     /* Page Contents */
     let content = document.getElementById("content");
-
+    
     let landingPage = elementBuilder("section", "landing-page", content);
     let header = elementBuilder("header", "page-head",  landingPage);
     header.textContent = "Vito's Pizzeria"
     let article = elementBuilder("article", "landing-article", landingPage);
+    
+    article.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    
     let landingImage = elementBuilder("img", "landing-image", landingPage);
-    landingImage.src = ``;
+    landingImage.src = `http://via.placeholder.com/640x360`;
 
 }
 
