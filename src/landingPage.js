cc0281c6113512ace1e9f9edc38aabc41fc51af7
David Stockdale <dstock3@gmail.com>
2021-08-11 23:06:02 -0500

    Added header content.

--- src/landingPage.js
+++ src/landingPage.js
@@ -13,6 +13,7 @@ const pageLoad = () => {
 
     let landingPage = elementBuilder("section", "landing-page", content);
     let header = elementBuilder("header", "page-head",  landingPage);
+    header.textContent = "Vito's Pizzeria"
     let article = elementBuilder("article", "landing-article", landingPage);
     let landingImage = elementBuilder("img", "landing-image", landingPage);
     landingImage.src = ``;
