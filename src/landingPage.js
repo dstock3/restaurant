fc423831c79e1d1ccda38474f994cb8ba9b34582
David Stockdale <dstock3@gmail.com>
2021-08-11 23:04:03 -0500

    Added basic elements for landing image, added some notation to delineate DOM manipulation from HTML elements.

--- src/landingPage.js
+++ src/landingPage.js
@@ -13,8 +13,10 @@ const pageLoad = () => {
 
     let landingPage = elementBuilder("section", "landing-page", content);
     let header = elementBuilder("header", "page-head",  landingPage);
+    let article = elementBuilder("article", "landing-article", landingPage);
+    let landingImage = elementBuilder("img", "landing-image", landingPage);
+    landingImage.src = ``;
 
-    let heroImage = ``;
 }
 
 export { pageLoad }
