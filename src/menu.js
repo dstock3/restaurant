f6ca8f9e7c2ab290206e8cd3631ac3df56120027
David Stockdale <dstock3@gmail.com>
2021-08-20 17:51:13 -0500

    Added some pic pics to the menu section.

--- src/menu.js
+++ src/menu.js
@@ -18,28 +18,28 @@ const menu = () => {
 
     const classic = foodItem(
         "Vito's Classic",
-        "http://via.placeholder.com/640x360",
+        "../dist/images/cheese.jpg",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "classic",
     );
 
     const pep = foodItem(
         "Pepperoni Pizza",
-        "http://via.placeholder.com/640x360",
+        "../dist/images/pepperoni.jpg",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "pep",
     );
 
     const veg = foodItem(
         "Vegetarian Pizza",
-        "http://via.placeholder.com/640x360",
+        "../dist/images/veg.jpg",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "veg",
     );
 
     const hawaiian = foodItem(
         "Hawaiian Pizza",
-        "http://via.placeholder.com/640x360",
+        "../dist/images/haw.jpg",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "hawaii",
     );
