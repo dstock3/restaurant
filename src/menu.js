422ea053acf9529ff8947cc9ed031dbecebfce84
David Stockdale <dstock3@gmail.com>
2021-08-22 16:32:50 -0500

    Added more pics

--- src/menu.js
+++ src/menu.js
@@ -46,14 +46,14 @@ const menu = () => {
 
     const beef = foodItem(
         "Italian Beef",
-        "http://via.placeholder.com/640x360",
+        "../dist/images/beef.jpg",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "beef",
     );
 
     const sub = foodItem(
         "Meatball Sub",
-        "http://via.placeholder.com/640x360",
+        "../dist/images/meatball.jpg",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "sub"
     );
